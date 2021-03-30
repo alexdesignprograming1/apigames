1bbfa95c9a2f11d461ef6252d41f48b80f8e2256
Alex Silva <alexsilva10san@gmail.com>
2021-03-30 17:30:36 -0300

    Authecation API and JWT

--- index.js
+++ index.js
@@ -2,11 +2,36 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser"); 
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const JWTSecret = "FtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImp0aSI6IjdiZDRhZjQxLWU5MTQtNDY0Mi05MzA5L"
 
 app.use(cors());
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+function auth(req, res, next){
+  const authToken = req.headers['authorization'];
+  if(authToken != undefined){
+    const bearer = authToken.split(' ');
+    var token = bearer[1];
+
+    jwt.verify(token, JWTSecret, (err, data) => {
+      if(err){
+        res.status(401);
+        res.json({err:"Token inválido"});
+      }else {
+        req.token = token;
+        req.loggedUser = {id: data.id, email: data.email};
+        next();
+      }
+    })
+  }else{
+    res.status(401);
+    res.json({err: "Token inválido"});
+  }
+  next();
+}
 
 const DB = {
   games: [
@@ -28,15 +53,61 @@ const DB = {
       year: 2011,
       price: 45
     },
+  ],
+  users: [
+    {
+      "id": 39,
+      "firstname": "Kylynn",
+      "lastname": "Brenn",
+      "city": "Buffalo",
+      "country": "Haiti",
+      "countryCode": "SV",
+      "email": "Kylynn.Brenn@gmail.com",
+      "age": 24,
+      "password": "nodejs"
+    },
+    {
+      "id": 100,
+      "firstname": "Georgetta",
+      "lastname": "Francene",
+      "city": "Koror",
+      "country": "Guernsey",
+      "countryCode": "SR",
+      "email": "Georgetta.Francene@gmail.com",
+      "age": 81,
+      "password": "nodejs"
+    },
+    {
+      "id": 50,
+      "firstname": "Ayn",
+      "lastname": "Fitzsimmons",
+      "city": "Lisbon",
+      "country": "Saint Lucia",
+      "countryCode": "NF",
+      "email": "Ayn.Fitzsimmons@gmail.com",
+      "age": 99,
+      "password": "nodejs"
+    },
+    {
+      "id": 85,
+      "firstname": "Celestyna",
+      "lastname": "Corabella",
+      "city": "Hanover",
+      "country": "Georgia",
+      "countryCode": "VE",
+      "email": "Celestyna.Corabella@gmail.com",
+      "age": 45,
+      "password": "nodejs"
+    }
   ]
 }
 
-app.get("/games", (req, res) => {
+app.get("/games",auth, (req, res) => {
   res.statusCode = 200;
-  res.json(DB.games);
+  res.json({user: req.loggedUser,games: DB.games});
 });
 
-app.get("/game/:id", (req, res) => {
+app.get("/game/:id",auth, (req, res) => {
   
   if(isNaN(req.params.id)){
     res.sendStatus(400);
@@ -54,7 +125,7 @@ app.get("/game/:id", (req, res) => {
   }
 })
 
-app.post("/game", (req, res) => {
+app.post("/game",auth, (req, res) => {
   const {title, price, year} = req.body;
 
   DB.games.push({
@@ -68,7 +139,7 @@ app.post("/game", (req, res) => {
 
 })
 
-app.delete("/game/:id", (req, res) => {
+app.delete("/game/:id",auth, (req, res) => {
 
   if(isNaN(req.params.id)){
     res.sendStatus(400);
@@ -85,7 +156,7 @@ app.delete("/game/:id", (req, res) => {
   }
 })
 
-app.put("/game/:id", (req, res) => {
+app.put("/game/:id",auth, (req, res) => {
   if(isNaN(req.params.id)){
     res.sendStatus(400);
   }else{
@@ -114,6 +185,42 @@ app.put("/game/:id", (req, res) => {
       res.sendStatus(404);
     }
   }
+})
+
+app.post("/auth",auth, (req, res) => {
+  const  {email, password} = req.body;
+
+  if(email != undefined){
+
+    const user = DB.users.find(dados => dados.email == email);
+
+      if(user != undefined){
+
+        
+        if (user.password == password) {
+          jwt.sign({id: user.id, email: user.email}, JWTSecret,{expiresIn: '48h'},(err, token) => {
+            if(err){
+              res.status(400);
+              res.json({err: "Falha interna"});
+            }else{
+              res.status(200);
+              res.json({token: token});
+            }
+          });
+        }else{
+          res.status (401);
+          res.json({err: "Credenciais inválidas!"});
+        }
+      }else{
+        res.status(404);
+        res.json({ err: "O E-mail inválido! não existe na base de dados!"});
+      }
+
+  }else{
+    res.status(400);
+    res.json({err: "O E-mail enviado é inválido!"})
+  }
+
 })
 
 app.listen("8080", () => {
